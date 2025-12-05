20a6713aa07d9a400633fd41128403dd5a6a34b6
Angel Blanco <angel.softworks@gmail.com>
2025-12-05 17:05:06 -0400

    Restoring TypedKey equality to its previous form

--- src/main/scala/com/github/novamage/typedmap/TypedKey.scala
+++ src/main/scala/com/github/novamage/typedmap/TypedKey.scala
@@ -8,21 +8,12 @@ import scala.annotation.targetName
   */
 abstract class TypedKey[A] {
 
-  private val fullyQualifiedName = getClass.getName
-
   /** Returns a [[TypedEntry]] by combining this key with the passed in value
     *
     * @param value The value to associate with this key
     */
   @targetName("arrow")
   def -> (value: A): TypedEntry[A] = new TypedEntry(this, value)
-
-  override def hashCode(): Int = fullyQualifiedName.hashCode
-
-  override def equals(obj: Any): Boolean = obj match {
-    case that: TypedKey[_] => this.fullyQualifiedName == that.fullyQualifiedName
-    case _                 => false
-  }
 
   override def toString: String = getClass.getSimpleName.replace("$", "")
 }
